e8057b00b52fb27c27dcbfb1c8ff8a806d1b5c51
XanMead <x.m.mead@gmail.com>
2015-04-14 18:30:02 -0400

    Added guess handling; started feedback functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 var HC_MIN = 1,
     HC_MAX = 100;
 var DEFAULT_FEEDBACK = "Make your Guess!";
+var FEEDBACK = ["Very Hot!", "Hot", "Warm", "Cool", "Cold", "Ice Cold..."];
 
 /* GAME VARS */
 var secretNumber;
@@ -34,6 +35,9 @@ $(document).ready(function(){
   			$('#guessButton').click();
   		}
   	});
+
+    // Set up the game
+    newGame();
 });
 
 function newGame() {
@@ -61,9 +65,27 @@ function generateSecretNumber() {
 	secretNumber = rng(HC_MIN, HC_MAX);
 }
 
-
 function makeGuess() {
+  var guess = $('#userGuess').val();
+  guess = parseInt(guess);
+  if (guess >= HC_MIN && guess <= HC_MAX) {
+    getFeedback(guess);
+  }
+  else {
+    alert('Please enter a number between 1 and 100.');
+  }
+  $('#userGuess').val('');
+}
 
+function getFeedback(guess) {
+  // Get the absolute difference between guess and number
+  var feedbackIndex = Math.abs(secretNumber - guess);
+  // Divide by 10 to find index
+  feedbackIndex = Math.floor(feedbackIndex/10);
+  if (feedbackIndex >= FEEDBACK.length) {
+    feedbackIndex = FEEDBACK.length - 1;
+  }
+  setFeedback(FEEDBACK[feedbackIndex]);
 }
 
 /* Borrowed from this JSFiddle: http://jsfiddle.net/alanwsmith/GfAhy/ */
