2767b125a9dc79ce42fef6e48ca48659cd4d3419
XanMead <x.m.mead@gmail.com>
2015-04-14 13:44:17 -0400

    established global vars and constants; wrote newGame procedure

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
+/* GAME PARAMETERS */
+var HC_MIN = 1,
+    HC_MAX = 100;
+var DEFAULT_FEEDBACK = "Make your Guess!";
+
+/* GAME VARS */
+var secretNumber;
+var numGuesses;
+
 
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -12,6 +20,37 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+
+
 });
 
+function newGame() {
+	setFeedback(DEFAULT_FEEDBACK);
+	clearUserGuess();
+	clearGuessList();
+	resetGuessCount();
+	generateSecretNumber();
+}
 
+function clearUserGuess() {
+	$('#userGuess').val('');
+}
+
+function clearGuessList() {
+	$('#guessList').children().remove();
+}
+
+function resetGuessCount() {
+	$('#count').text('0');
+	numGuesses = 0;
+}
+
+function generateSecretNumber() {
+	secretNumber = rng(HC_MIN, HC_MAX);
+}
+
+
+/* Borrowed from this JSFiddle: http://jsfiddle.net/alanwsmith/GfAhy/ */
+function rng(min, max) {
+	return Math.floor(Math.random()*(max-min+1)+min);
+}
