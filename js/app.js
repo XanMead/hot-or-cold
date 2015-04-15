2a24815d7d3c7d995c289688464b1bfa30f508dc
XanMead <x.m.mead@gmail.com>
2015-04-14 22:31:09 -0400

    refactored feedback function and helpers; added guessList functionality

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var HC_MIN = 1,
     HC_MAX = 100;
 var DEFAULT_FEEDBACK = "Make your Guess!";
 var FEEDBACK = ["Very Hot!", "Hot", "Warm", "Cool", "Cold", "Ice Cold..."];
+var WIN_TEXT = "You guessed it!";
 
 /* GAME VARS */
 var secretNumber;
@@ -11,6 +12,9 @@ var numGuesses;
 
 $(document).ready(function(){
 	
+    /* nAPALM uNNECESSARY html5 cAUSE wE gOT tHAT cOVERED*/
+    $('#userGuess').removeAttr("required");
+
 	/*--- Display information modal box ---*/
   	$('.what').click(function() {
     	$('.overlay').fadeIn(1000);
@@ -40,6 +44,7 @@ $(document).ready(function(){
     newGame();
 });
 
+/*** GAME SET-UP ***/
 function newGame() {
 	setFeedback(DEFAULT_FEEDBACK);
 	clearUserGuess();
@@ -57,19 +62,21 @@ function clearGuessList() {
 }
 
 function resetGuessCount() {
-	$('#count').text('0');
+  setNumGuesses(0);
 	numGuesses = 0;
 }
 
 function generateSecretNumber() {
 	secretNumber = rng(HC_MIN, HC_MAX);
+  console.log("PSSST. THE SECRET NUMBER IS " + secretNumber);
 }
 
+/*** CORE FUNCTIONALITY ***/
 function makeGuess() {
   var guess = $('#userGuess').val();
   guess = parseInt(guess);
   if (guess >= HC_MIN && guess <= HC_MAX) {
-    getFeedback(guess);
+    giveFeedbackOn(guess);
   }
   else {
     alert('Please enter a number between 1 and 100.');
@@ -77,15 +84,42 @@ function makeGuess() {
   $('#userGuess').val('');
 }
 
-function getFeedback(guess) {
-  // Get the absolute difference between guess and number
-  var feedbackIndex = Math.abs(secretNumber - guess);
-  // Divide by 10 to find index
-  feedbackIndex = Math.floor(feedbackIndex/10);
-  if (feedbackIndex >= FEEDBACK.length) {
-    feedbackIndex = FEEDBACK.length - 1;
+function giveFeedbackOn(guess) {
+  var feedback;
+  if (guess == secretNumber) {
+    feedback = WIN_TEXT;
   }
-  setFeedback(FEEDBACK[feedbackIndex]);
+  else {
+    // Get the absolute difference between guess and number
+    var feedbackIndex = Math.abs(secretNumber - guess);
+    // Divide by 10 to find index
+    feedbackIndex = Math.floor(feedbackIndex/10);
+    if (feedbackIndex >= FEEDBACK.length) {
+      feedbackIndex = FEEDBACK.length - 1;
+    }
+    feedback = FEEDBACK[feedbackIndex];
+  }
+  setFeedback(feedback);
+  incrementGuesses();
+  addToGuessList(guess);
+}
+
+/*** HELPER FUNCTIONS ***/
+function incrementGuesses() {
+  numGuesses++;
+  setNumGuesses(numGuesses);
+}
+
+function setFeedback(message) {
+  $('#feedback').text(message);
+}
+
+function setNumGuesses(num) {
+  $('#count').text(num);
+}
+
+function addToGuessList(guess) {
+  $('<li></li>').appendTo('#guessList').text(guess);
 }
 
 /* Borrowed from this JSFiddle: http://jsfiddle.net/alanwsmith/GfAhy/ */
